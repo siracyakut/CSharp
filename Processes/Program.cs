de8ac36acd8339c0501762dadef205bfa0c55014
flareoNNN <sirac1234@gmail.com>
2020-07-16 01:05:32 +0300

    upload

---
+++ Processes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Processes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] hafiza = Process.GetProcesses();
+            foreach (Process prc in hafiza)
+            {
+                Echo("Uygulama: ", ConsoleColor.Green);
+                Echo($"{prc.ProcessName}\n", ConsoleColor.White);
+            }
+            Console.ReadKey();
+        }
+        static void Echo(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Processes/README.md
@@ -0,0 +1,7 @@
+# Processes
+
+Windows üzerinde çalışan tüm uygulamaları gösterir.
+
+# Görüntü
+
+![Görüntü](https://www.upload.ee/image/12014853/Screenshot_9.png)
