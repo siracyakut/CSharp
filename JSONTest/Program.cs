ddf760749a7c4a313046ea0a5f77c5d680427dd6
flareoNNN <sirac1234@gmail.com>
2020-07-23 03:21:29 +0300

    upload

---
+++ JSONTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace jsonTest
+{
+    class Program
+    {
+        public class veri
+        {
+            [JsonProperty(PropertyName = "id")]
+            public string ID { get; set; }
+
+            [JsonProperty(PropertyName = "channel")]
+            public string Name { get; set; }
+
+            [JsonProperty(PropertyName = "title")]
+            public string Title { get; set; }
+
+            [JsonProperty(PropertyName = "full_link")]
+            public string Link { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            string str = "[{\"id\":\"-UKfDXK2ADk\",\"channel\":\"Test Divertidos\",\"title\":\"\u00bfCU\u00c1L ES TU TALENTO? Test Divertidos de verialidad\",\"full_link\":\"https://youtube.com/watch?v=-UKfDXK2ADk\"},{\"id\":\"t-mQvJ9Onhs\",\"channel\":\"Test Ex\",\"title\":\"Experiment Car vs Pepsi | Crushing crunchy &amp; soft things by car | Test Ex\",\"full_link\":\"https://youtube.com/watch?v=t-mQvJ9Onhs\"}]";
+            var degisken = JsonConvert.DeserializeObject<List<veri>>(str);
+
+            foreach (var veri in degisken)
+            {
+                Console.WriteLine(
+                    string.Format("ID: {0}, Channel: {1}, Title: {2}, Link: {3}",
+                                    veri.ID,
+                                    veri.Name,
+                                    veri.Title,
+                                    veri.Link));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ JSONTest/README.md
@@ -0,0 +1,14 @@
+# Json Array Test
+
+Satır:
+
+```
+[{"id":"-UKfDXK2ADk","channel":"Test Divertidos","title":"\u00bfCU\u00c1L ES TU TALENTO? Test Divertidos de Personalidad","full_link":"https:\/\/youtube.com\/watch?v=-UKfDXK2ADk"},{"id":"t-mQvJ9Onhs","channel":"Test Ex","title":"Experiment Car vs Pepsi | Crushing crunchy &amp; soft things by car | Test Ex","full_link":"https:\/\/youtube.com\/watch?v=t-mQvJ9Onhs"}]
+```
+
+Çıktı:
+
+```
+ID: -UKfDXK2ADk, Channel: Test Divertidos, Title: ¿CUÁL ES TU TALENTO? Test Divertidos de verialidad, Link: https://youtube.com/watch?v=-UKfDXK2ADk
+ID: t-mQvJ9Onhs, Channel: Test Ex, Title: Experiment Car vs Pepsi | Crushing crunchy &amp; soft things by car | Test Ex, Link: https://youtube.com/watch?v=t-mQvJ9Onhs
+```
